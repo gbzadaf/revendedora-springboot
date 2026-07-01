eb208749e2bed7a53a2c5b91d717b2d3f500484e
gbzadaf <fonsecagb23@gmail.com>
2026-07-01 20:03:49 -0300

    first commit

---
+++ src/main/java/com/gabrielf/revendedora_api/RevendedoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielf.revendedora_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevendedoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevendedoraApiApplication.class, args);
+	}
+
+}
