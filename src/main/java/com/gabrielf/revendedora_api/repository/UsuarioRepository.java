0c5e00d2eb4bdad62a65a22d368894f776ce7146
gbzadaf <fonsecagb23@gmail.com>
2026-07-11 15:54:17 -0300

    fix: usuario repository

--- src/main/java/com/gabrielf/revendedora_api/repository/UsuarioRepository.java
+++ src/main/java/com/gabrielf/revendedora_api/repository/UsuarioRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UsuarioRepository extends JpaRepository<UUID, Usuario> {
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
     Optional<Usuario> findByLogin(String login);
 
